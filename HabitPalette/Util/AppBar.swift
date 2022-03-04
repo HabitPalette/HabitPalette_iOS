63d883dca883fde6ee8f6b53b1ce7206f7dffe44
Eunji <eunjilim218@qarasoft.com>
2022-03-04 11:03:54 +0900

    [feat] #3 app bar 추가

---
+++ HabitPalette/Util/AppBar.swift
@@ -0,0 +1,23 @@
+//
+//  AppBar.swift
+//  HabitPalette
+//
+//  Created by qara_macbookpro on 2022/03/04.
+//
+
+import UIKit
+
+class AppBar: BaseView {
+    
+    override func setupLayout() {
+        
+    }
+    
+    override func setupConstraints() {
+        
+    }
+    
+    override func setData() {
+        
+    }
+}
